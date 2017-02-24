8ebfc3cae1c96d5b3f98347ffcb211438ac7be1a
Morten Liknes <mliknes@hotmail.com>
2017-02-24 13:33:46 +0000

    AttendeeValidator

--- Code/CheckPointSourceCode/CheckPoint/CheckPointModel/Validation/AttendeeValidator.cs
+++ Code/CheckPointSourceCode/CheckPoint/CheckPointModel/Validation/AttendeeValidator.cs
@@ -5,38 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 using CheckPointModel.Entities;
 using CheckPointModel.Utilities;
+using CheckPointCommon.Enums;
 namespace CheckPointModel.Validation
 {
     public class AttendeeValidator:Validator<AttendeeModel>
     {
-        private List<object> PropertyList;
+        //not tested
 
-        public List<object> FillPropertyList(AttendeeModel attendee)
-        {
-            PropertyList = new List<object>();
 
-            PropertyList.Add(attendee.AppointmentId);
-            PropertyList.Add(attendee.PersonalNote);
-            PropertyList.Add(attendee.StatusId);
-            PropertyList.Add(attendee.TagId);
-            PropertyList.Add(attendee.TimeAttended);
-
-            return PropertyList;
-
-        }
+        /// <summary>
+        /// Validates the AttendeeModel proporties
+        /// </summary>
+        /// <param name="attendee"></param>
         public override void CheckForBrokenRules(AttendeeModel attendee)
         {            
             if (!ValidateIntergerInput.IsIntergerValid(attendee.AppointmentId))
                 base.AddBrokenRule("AppointmentId is not valid");                       
 
             if(!ValidateStringInput.IsStringValid(attendee.TagId))
-                base.AddBrokenRule("TagId Is not valid!");
+                base.AddBrokenRule("TagId Is not valid");
 
+            if (!Enum.IsDefined(typeof(AttendeeStatus), attendee.StatusId))
+                base.AddBrokenRule("StatusId is not valid");
+
+            if (!ValidateStringInput.IsStringValid(attendee.TagId))
+                base.AddBrokenRule("TagId is not valid");
+
+            if (!(ValidateTimeAttended(attendee.TimeAttended, attendee.StatusId)))
+                base.AddBrokenRule("TimeAttended is not Valid");
         }
 
-        private bool ValidateStatusId(int statusId)
-        {
+        private bool ValidateTimeAttended(string timeAttended, int statusId)
+        {       
+            if (!(statusId == (int)AttendeeStatus.ObligHasAttended || statusId == (int)AttendeeStatus.HasAttended))
+            {
+                return false;
+            }
+
+            if(!ValidateStringInput.IsStringValid(timeAttended))
+            {
+                return false;
+            }
+
+            if (!ValidateDateInput.IsDateValidate(timeAttended))            
+                {
+                return false;
+                }
+
             return true;
-        }
+                                                   
+         }
+
+
     }
 }
