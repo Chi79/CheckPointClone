51ff8580c3ef25e2199b12b6f2c41b022f88ab3a
tonje_ <32202134+Chi79@users.noreply.github.com>
2017-03-02 23:41:37 +0000

    Added LazyLoading to Factory

--- CheckPoint/CheckPointModel/Factories/JobFactory.cs
+++ CheckPoint/CheckPointModel/Factories/JobFactory.cs
@@ -16,18 +16,26 @@ namespace CheckPointModel.Factories
     {
         private  IHandleAppointments<APPOINTMENT, SaveResult> _handler;
 
-        private  Dictionary<DbAction, JobServiceBase> Jobs = new Dictionary<DbAction, JobServiceBase>();
+        private Dictionary<DbAction, JobServiceBase> Jobs; 
         public JobFactory(IHandleAppointments<APPOINTMENT, SaveResult> handler)
         {
             _handler = handler;
-      
-            Jobs.Add(DbAction.Create, new CreateJob(handler));
-            Jobs.Add(DbAction.Delete, new DeleteJob(handler));
-            Jobs.Add(DbAction.Update, new UpdateJob(handler));
+        }
+        public void LoadDictionary(IHandleAppointments<APPOINTMENT, SaveResult> handler)
+        {
+            if(Jobs == null)
+            {
+                Jobs = new Dictionary<DbAction, JobServiceBase>();
+
+                Jobs.Add(DbAction.Create, new CreateJob(handler));
+                Jobs.Add(DbAction.Delete, new DeleteJob(handler));
+                Jobs.Add(DbAction.Update, new UpdateJob(handler));
+            }
         }
        
         public JobServiceBase Create(DbAction action)
         {
+            LoadDictionary(_handler);
             return Jobs[action];
         }
     }
