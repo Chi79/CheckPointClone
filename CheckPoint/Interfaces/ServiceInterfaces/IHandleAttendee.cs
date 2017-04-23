9227be40d8e3f49518af55a7ded4a05ebf9f92b1
Morten Liknes <mliknes@hotmail.com>
2017-04-23 17:35:12 +0000

    possible to apply to an appointment

---
+++ CheckPoint/Interfaces/ServiceInterfaces/IHandleAttendee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPointCommon.ServiceInterfaces
+{
+   public interface IHandleAttendee:IHandler
+    {
+    }
+}
